daf131fbc8cc463c63a03be3aeda417c49f9e289
awsmith <merryarronoftheshire@gmail.com>
2021-09-18 10:35:31 -0700

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -9,4 +9,4 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'wrong.html')
+        self.assertTemplateUsed(response, 'home.html')
